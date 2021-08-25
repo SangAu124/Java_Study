231d4c437330bd85be3d8906c3897c9e68f6ce35
김상은 <81676542+SangAu124@users.noreply.github.com>
2021-08-25 19:31:55 +0900

    자바 정리
    자바 정리

---
+++ Hello/src/calculator/calculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+public class calculator {
+    public int add(int a, int b) {
+        return (a + b);
+    }
+    public int min(int a, int b){
+        return (a - b);
+    }
+    public int mul(int a, int b){
+        return (a * b);
+    }
+    public int div(int a, int b){
+        return ((a / b) + ( a % b));
+    }
+}
---
+++ Hello/src/calculator/main.java
@@ -0,0 +1,30 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        calculator calculator = new calculator();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("정수 계산기 입니다. 정수 하나를 입력하세요");
+        int a = scanner.nextInt();
+        System.out.print("사용할 수식을 입력하세요");
+        String s = scanner.nextLine();
+        System.out.print("정수 하나를 입력하세요");
+        int b = scanner.nextInt();
+
+        if (s == "+"){
+            System.out.println(calculator.add(a, b));
+        }
+        else if (s == "-"){
+            System.out.println(calculator.min(a, b));
+        }
+        else if ( s == "*"){
+            System.out.println(calculator.mul(a, b));
+        }
+        else {
+            System.out.println(calculator.div(a, b));
+        }
+    }
+}
